b351cd1f7dffe03f4d8028f30cbd520bea91b133
kavenGw <kavenGw@gmail.com>
2026-08-04 15:18:47 +0800

    remote openclaw github_release

--- app/config/github_releases.py
+++ app/config/github_releases.py
@@ -14,12 +14,6 @@ GITHUB_RELEASE_REPOS = [
         'emoji': '⚡',
     },
     {
-        'key': 'openclaw',
-        'repo': 'openclaw/openclaw',
-        'name': 'OpenClaw',
-        'emoji': '🦀',
-    },
-    {
         'key': 'oh-my-claudecode',
         'repo': 'yeachan-heo/oh-my-claudecode',
         'name': 'oh-my-claudecode',
